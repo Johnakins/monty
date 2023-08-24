80507ffdc99fc2648023f5c718116bf94a3427ac
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-08-24 01:57:02 -0700

    push functions

--- push.c
+++ push.c
@@ -1,16 +1,65 @@
 #include "monty.h"
+
 /**
- * push - removes a memory
- * @stack: the pointer
- * @value: the values of the memory
- * Return: void
+ * push - Adds a new node at the beginning of the stack
+ * @stack: Pointer to the head of the stack
+ * @param: The value to be added to the stack
+ *
+ * Return: Nothing
  */
-void push(Stack *stack, int value)
+void push(stack_t **stack, unsigned int param)
 {
-	if (stack->top == STACK_SIZE)
+	stack_t *new_node = NULL;
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+		handle_error(ERR_BAD_MALL, NULL, 0, NULL);
+
+	new_node->n = param;
+	if (*stack)
 	{
-		fprintf(stderr, "Stack overflow\n");
-		exit(EXIT_FAILURE);
+		new_node->next = *stack;
+		new_node->prev = (*stack)->prev;
+		(*stack)->prev = new_node;
+		*stack = new_node;
+		return;
 	}
-	stack->data[stack->top++] = value;
+
+	new_node->next = *stack;
+	new_node->prev = NULL;
+	*stack = new_node;
 }
+
+/**
+ * push_queue - Adds a new node at the end of the stack
+ * @stack: Pointer to the head of the stack
+ * @param: The value to be added to the stack
+ *
+ * Return: Nothing
+ */
+void push_queue(stack_t **stack, unsigned int param)
+{
+	stack_t *current = NULL, *new_node = NULL;
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+		handle_error(ERR_BAD_MALL, NULL, 0, NULL);
+
+	new_node->n = param;
+	if (*stack)
+	{
+		current = *stack;
+		while (current->next != NULL)
+			current = current->next;
+
+		new_node->next = NULL;
+		new_node->prev = current;
+		current->next = new_node;
+		return;
+	}
+
+	new_node->next = *stack;
+	new_node->prev = NULL;
+	*stack = new_node;
+}
+
